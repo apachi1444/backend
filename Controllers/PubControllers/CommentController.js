83bf35f09cfbf068cb89a7ae406d57ca5903cdce
sammy <samuelboateng472.com>
2022-02-14 14:28:41 +0100

    Implenting delete and edit comment controllers

--- Controllers/PubControllers/CommentController.js
+++ Controllers/PubControllers/CommentController.js
@@ -1,8 +1,21 @@
 const ObjectId = require("mongoose").Types.ObjectId;
 const Pub = require("../../Models/pubModel");
-module.exports.editCommentPost = () => {};
+module.exports.editCommentPost = async(req,res) => {
+  const {ID} = req.params;
+  const newText = req.body.newText;
+  if(!ObjectID.isValid(ID)){
+  return res.status(400).send("ID does not exist: " + req.params.id);
+  };
+  await Pub.update({" comments.commenterId" : ID },{$set : {"comments.$.text" : newText} })
+    .then( result =>{
+       res.status(200).json({message:"Comment deleted successfully"});                                            
+     })
+     .catch(e=>{
+       res.status(400).send(err);
+     }); 
+};
 
-module.exports.commentPost = (res, req) => {
+module.exports.commentPost = (req, res) => {
   if (!ObjectId.isValid(req.params.id)) {
     return res.status(400).send("ID unknown : " + req.params.id);
   }
@@ -34,4 +47,16 @@ module.exports.commentPost = (res, req) => {
   }
 };
 
-module.exports.deleteCommentPost = () => {};
+module.exports.deleteCommentPost = async(req,res) => {
+  const {ID} = req.params;
+  if(!ObjectID.isValid(ID)){
+    return res.status(400).send("ID does not exist: " + req.params.id);
+  };
+  await Pub.findOneAndUpdate({" comments.commenterId" : ID },{$pull : { comments : { commenterId : ID } }})
+      .then(result=>{
+         res.status(200).json({message:"Comment deleted successfully"});                                            
+       })
+       .catch(e=>{
+         res.status(400).send(err);
+       });                                                                                                                                                                                      
+};
